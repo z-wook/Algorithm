6f291ac9fbf9530f1d458a175d9eb15936204326
HAN JIWOOK <jiwook.han.dev@gmail.com>
2024-01-06 03:53:27 +0900

    Update 공 넣기.swift

--- 백준/Bronze/10810. 공 넣기/공 넣기.swift
+++ 백준/Bronze/10810. 공 넣기/공 넣기.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-var baskets = Array(repeating: 0, count: input[0])
+let data = readLine()!.split(separator: " ").map { Int($0)! }
+var baskets = Array(repeating: 0, count: data[0])
 
-for _ in 0..<input[1] {
-    let aaa = readLine()!.split(separator: " ").map { Int($0)! }
-    let i = aaa[0]
-    let j = aaa[1]
-    let k = aaa[2]
+for _ in 0..<data[1] {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let i = input[0]
+    let j = input[1]
+    let k = input[2]
     
     for index in i...j {
         baskets[index - 1] = k
