cee27a0b620306981917b534a7270f660aae9a87
HAN JIWOOK <jiwook.han.dev@gmail.com>
2024-01-07 12:47:23 +0900

    [Bronze II] Title: 상수, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.swift
+++ 백준/Bronze/2908. 상수/상수.swift
@@ -1,13 +1,7 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { String($0) }
-var newList: [Int] = []
+let input = readLine()!.split { $0 == " " }
+    .map { String($0.reversed()) }
+    .max()!
 
-input.forEach {
-    var result = $0.map { String($0) }
-    result.reverse()
-    let newStr = result.joined()
-    newList.append(Int(newStr)!)
-}
-
-print(newList.max()!)
+print(input)
